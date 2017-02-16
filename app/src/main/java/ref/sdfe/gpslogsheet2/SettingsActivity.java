c6d14916db9801e325326875c737f0c6f2f67c25
oli-dj <oldjo@sdfe.dk>
2017-02-16 18:20:44 +0100

    Changed preferences (settings) to include a credential page for setting usernames/passwords etc.

--- app/src/main/java/ref/sdfe/gpslogsheet2/SettingsActivity.java
+++ app/src/main/java/ref/sdfe/gpslogsheet2/SettingsActivity.java
@@ -171,7 +171,8 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
         return PreferenceFragment.class.getName().equals(fragmentName)
                 || GeneralPreferenceFragment.class.getName().equals(fragmentName)
                 || DataSyncPreferenceFragment.class.getName().equals(fragmentName)
-                || NotificationPreferenceFragment.class.getName().equals(fragmentName);
+                || NotificationPreferenceFragment.class.getName().equals(fragmentName)
+                || CredentialsPreferenceFragment.class.getName().equals(fragmentName);
     }
 
     /**
@@ -252,6 +253,40 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
             // updated to reflect the new value, per the Android Design
             // guidelines.
             bindPreferenceSummaryToValue(findPreference("sync_frequency"));
+        }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            int id = item.getItemId();
+            if (id == android.R.id.home) {
+                startActivity(new Intent(getActivity(), SettingsActivity.class));
+                return true;
+            }
+            return super.onOptionsItemSelected(item);
+        }
+    }
+    /**
+     * This fragment shows data and sync preferences only. It is used when the
+     * activity is showing a two-pane settings UI.
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    public static class CredentialsPreferenceFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.pref_credentials);
+            setHasOptionsMenu(true);
+
+            // Bind the summaries of prefs. When their values change, their summaries are
+            // updated to reflect the new value, per the Android Design
+            // guidelines.
+            bindPreferenceSummaryToValue(findPreference("credentials_ftp_user"));
+            // OLDJO: Didn't want to display the passwords though! :D
+            // bindPreferenceSummaryToValue(findPreference("credentials_ftp_password"));
+            bindPreferenceSummaryToValue(findPreference("credentials_ftp_host"));
+            bindPreferenceSummaryToValue(findPreference("credentials_ftp_path"));
+            bindPreferenceSummaryToValue(findPreference("credentials_kms_user"));
+            //bindPreferenceSummaryToValue(findPreference("credentials_kms_password"));
         }
 
         @Override
